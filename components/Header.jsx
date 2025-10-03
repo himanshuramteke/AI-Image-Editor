5961a25e7ae97af3c51e2f2ca20b460f86a6b8f5
Himanshu Ramteke <himanshuramteke007@gmail.com>
2025-10-03 22:38:57 +0530

    added schema.js

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,18 +1,21 @@
 "use client";
 
-import {
-  SignedIn,
-  SignedOut,
-  SignInButton,
-  SignUpButton,
-  UserButton,
-} from "@clerk/nextjs";
+import { SignInButton, SignUpButton, UserButton } from "@clerk/nextjs";
 import Image from "next/image";
 import Link from "next/link";
+import { useStoreUser } from "@/hooks/use-store-user";
 import { usePathname } from "next/navigation";
+import { BarLoader } from "react-spinners";
+import { Authenticated, Unauthenticated } from "convex/react";
 
 const Header = () => {
   const path = usePathname();
+
+  const { isLoading } = useStoreUser();
+
+  if (path.includes("/editor")) {
+    return null; //hide the editor page
+  }
 
   return (
     <header className="fixed top-6 left-1/2 transform -translate-x-1/2 z-50 text-nowrap">
@@ -51,18 +54,24 @@ const Header = () => {
         )}
 
         <div className="flex items-center gap-3 ml-10 md:ml-20">
-          <SignedOut>
+          <Unauthenticated>
             <SignInButton />
             <SignUpButton>
               <button className="bg-[#6c47ff] text-ceramic-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
                 Sign Up
               </button>
             </SignUpButton>
-          </SignedOut>
-          <SignedIn>
+          </Unauthenticated>
+          <Authenticated>
             <UserButton />
-          </SignedIn>
+          </Authenticated>
         </div>
+
+        {isLoading && (
+          <div className="fixed bottom-0 left-0 w-full z-40 flex justify-center">
+            <BarLoader width={"95%"} color="#06b6d4" />
+          </div>
+        )}
       </div>
     </header>
   );
