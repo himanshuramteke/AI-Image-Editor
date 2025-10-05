175db8f73bbdb959bc3bf713a5d13996a7f068cd
Himanshu Ramteke <himanshuramteke007@gmail.com>
2025-10-05 22:21:08 +0530

    features component and use-landing hook added

---
+++ hooks/use-landing.jsx
@@ -0,0 +1,20 @@
+"use client";
+
+import { useEffect, useRef, useState } from "react";
+
+export const useIntersectionObserver = (threshold = 0.1) => {
+  const [isVisible, setIsVisible] = useState(false);
+  const ref = useRef();
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      ([entry]) => setIsVisible(entry.isIntersecting),
+      { threshold }
+    );
+
+    if (ref.current) observer.observe(ref.current);
+    return () => observer.disconnect();
+  }, [threshold]);
+
+  return [ref, isVisible];
+};
